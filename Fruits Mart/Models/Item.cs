2ad89505076cdb7c50ead01feca768948b9cdec8
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-05-02 21:44:44 +0600

    Create Model for Database

---
+++ Fruits Mart/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace Fruits_Mart.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set;}
+
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }  
+        
+        public int Quantity { get; set; }   
+
+
+    }
+}
