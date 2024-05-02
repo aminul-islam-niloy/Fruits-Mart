1b064cf3a567723b258b3095a0f282e5358dab28
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-05-02 23:18:51 +0600

    Poceed to Controller

---
+++ Fruits Mart/Controllers/ItemsController.cs
@@ -0,0 +1,16 @@
+﻿using Fruits_Mart.Data;
+using Fruits_Mart.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fruits_Mart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+    
+
+
+    }
+}
