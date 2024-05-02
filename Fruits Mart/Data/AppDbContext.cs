1b37a84193aa246038535368e60943a6bcb35578
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-05-02 21:57:01 +0600

    Database Contex set and configure

---
+++ Fruits Mart/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Fruits_Mart.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fruits_Mart.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options) { }
+       
+        public DbSet<Item> Items { get; set; }
+
+    }
+
+}
