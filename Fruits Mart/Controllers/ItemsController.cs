b8542eebf3c7b004afc830c01153478c21589ab9
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-05-03 01:32:12 +0600

    Items controller with error

--- Fruits Mart/Controllers/ItemsController.cs
+++ Fruits Mart/Controllers/ItemsController.cs
@@ -2,6 +2,7 @@
 using Fruits_Mart.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fruits_Mart.Controllers
 {
@@ -9,8 +10,79 @@ namespace Fruits_Mart.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
-    
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+        public ItemsController(AppDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = hostEnvironment;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> GetItems()
+        {
+            return _context.Items.ToList();
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Item> GetItem(int id)
+        {
+            var item = _context.Items.Find(id);
+            if (item== null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<Item> CreateItem(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateItem(int id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteItem(int id)
+        {
+            var item = _context.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
 
 
     }
+
+
+
+
+
+
 }
