e211d385e9a99f79855f3ca3fc1993e36c3accaf
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-11-04 00:26:16 +0600

    update connectionstring for deploy on free hosting

--- Fruits Mart/Program.cs
+++ Fruits Mart/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || !app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
